07325e13ab99d85b5e357fe5a45c00c124130e52
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-06 08:33:25 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootopenapispec/SpringbootOpenApiSpecApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootopenapispec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOpenApiSpecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOpenApiSpecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootopenapispec/SpringbootOpenApiSpecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootopenapispec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOpenApiSpecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
