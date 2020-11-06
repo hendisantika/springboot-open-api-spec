f306b0b1cbc3d3269b81d69c673c94237afe6762
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-06 08:52:21 +0700

    #18 - Add EmployeeController

---
+++ src/main/java/com/hendisantika/springbootopenapispec/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.hendisantika.springbootopenapispec.controller;
+
+import com.hendisantika.springbootopenapispec.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-open-api-spec
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/11/20
+ * Time: 08.52
+ */
+@RestController
+public class EmployeeController {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+
+    @Autowired
+    private EmployeeService employeeService;
+
+}
