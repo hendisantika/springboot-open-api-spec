b5c25a4cfc8cdd18463977f0a02001c6b7414bf4
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-06 08:45:07 +0700

    #12 - Add addNewEmployee in EmployeeServiceImpl

--- src/main/java/com/hendisantika/springbootopenapispec/service/EmployeeServiceImpl.java
+++ src/main/java/com/hendisantika/springbootopenapispec/service/EmployeeServiceImpl.java
@@ -1,12 +1,14 @@
 package com.hendisantika.springbootopenapispec.service;
 
 import com.hendisantika.springbootopenapispec.model.Employee;
+import com.hendisantika.springbootopenapispec.model.EmployeeRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +26,21 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     //we can integrate this with db, but as we only want to see swagger demo, we put data in map
     private final Map<String, Employee> employeeStore = new HashMap<>();
+
+    @Override
+    public Employee addNewEmployee(EmployeeRequest request) {
+
+        logger.debug("Add new employee to the employee store");
+        String id = UUID.randomUUID().toString();
+
+        Employee employee = new Employee();
+        employee.setFirstName(request.getFirstName());
+        employee.setLastName(request.getLastName());
+        employee.setId(id);
+
+        employeeStore.put(id, employee);
+        logger.debug("Employee Count {}", employeeStore.size());
+
+        return employee;
+    }
 }
