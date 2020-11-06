4c33ac54997465b330173afa4600babfa3fc6973
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-06 08:42:27 +0700

    #8 - Add Employee Service

---
+++ src/main/java/com/hendisantika/springbootopenapispec/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootopenapispec.service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-open-api-spec
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/11/20
+ * Time: 08.41
+ */
+public interface EmployeeService {
+}
